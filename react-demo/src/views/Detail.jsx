d3b668d4b12e7643ac709519259578bf0578ebf4
jikang <2991807077@qq.com>
2022-08-05 11:37:19 +0800

    www

--- react-demo/src/views/Detail.jsx
+++ react-demo/src/views/Detail.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSearchParams } from 'react-router-dom'
 export default function Detail() {
     const [searchParams] = useSearchParams()
-    console.log('参数', searchParams)  
+    console.log('参数www', searchParams)  
     return (
         <div>Detail</div>
     )
